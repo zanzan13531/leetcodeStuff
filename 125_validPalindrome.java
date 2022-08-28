2f39ab4bc1fcbe630b3c5184a62d6cd0b99f56b6
zanzan13531 <zanzan13531@gmail.com>
2022-08-27 19:15:29 -0700

    p125

---
+++ 125_validPalindrome.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        char[] stringy = s.toLowerCase().toCharArray();
+        int start = 0;
+        int fin = stringy.length - 1;
+        while (fin - start > 1) {
+            while (!Character.isLetterOrDigit(stringy[start])) {
+                start++;
+                if (fin - start <= 0) {
+                    return (true);
+                }
+            }
+            while (!Character.isLetterOrDigit(stringy[fin])) {
+                fin--;
+                if (fin - start <= 0) {
+                    return (true);
+                }
+            }
+            if (stringy[start] == stringy[fin]) {
+                start++;
+                fin--;
+            } else {
+                return (false);
+            }
+        }
+        if (fin - start <= 0) {
+            return (true);
+        } else if (!Character.isLetterOrDigit(stringy[start]) || !Character.isLetterOrDigit(stringy[fin])) {
+            return (true);
+        } else {
+            return (stringy[start] == stringy[fin]);
+        }
+    }
+}
